ff4ff3b918f2fbb2f358afbdeb373e5d865f4f8b
MohammadRezaNouri <69000683+MohammadRezaNouri@users.noreply.github.com>
2021-10-18 01:01:36 +0330

    Write average program

--- avg.cpp
+++ avg.cpp
@@ -8,6 +8,25 @@ using namespace std;
 
 int main()
 {
-
+	f1c("-1 to exit");
+	float n = 0, sum = 0, score, unit;
+	float avg;
+	while (1)
+	{
+		fc("Score");
+		cin >> score;
+		if (score == -1)
+		{
+			break;
+		}
+		fc("Unit");
+		cin >> unit;
+		n += unit;
+		sum += score * unit;
+	}
+	fc("avg");
+	f1c(sum / n);
+	getchar();
+	getchar();
 	ret0;
 }
